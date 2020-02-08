f7dc09f0c51a0bc860f211427d412269606677c4
CharlieChengtianyue <tianyue.cheng2002@gmail.com>
2020-02-08 23:10:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+chengtianyue.github.io
---
+++ site_config/home.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+
+export default {
+  'zh-cn': {
+    brand: {
+      brandName: '查理的后花园',
+      briefIntroduction: '科技、生活、我',
+      buttons: [
+        {
+          text: '关于我',
+          link: '/zh-cn/docs/me_zh.html',
+          type: 'primary',
+        },
+        {
+          text: '查看我的github',
+          link: 'https://github.com/chengtianyue',
+          type: 'normal',
+        },
+      ],
+    },
+    introduction: {
+      title: '关于本网站',
+      desc: '本网站主要发布个人对于深度学习领域的一些小制作和见解，定期更新一些最新资讯。当然也会有平时生活中的所见所闻和小部分生活视频。',
+      img: '/img/architecture.png',
+    },
+    features: {
+      title: '网站功能',
+      list: [
+        {
+          img: '/img/feature_transpart.png',
+          title: '深度学习',
+          content: '以神经网络为主',
+        },
+        {
+          img: '/img/feature_loadbalances.png',
+          title: '机器学习',
+          content: 'kaggle等网站的一些样例',
+        },
+        {
+          img: '/img/feature_service.png',
+          title: '数据分析',
+          content: 'kaggle热门帖子汇总',
+        },
+        {
+          img: '/img/feature_hogh.png',
+          title: '视频剪辑',
+          content: '记录生活',
+        },
+        {
+          img: '/img/feature_runtime.png',
+          title: '篮球',
+          content: '比赛资讯，球星资讯等',
+        },
+        {
+          img: '/img/feature_maintenance.png',
+          title: '日常',
+          content: '一个高中生的日常生活',
+        },
+      ],
+    },
+    start: {
+      title: '最新文章',
+      desc: '1',
+      img: '/img/quick_start.png',
+      button: {
+        text: '关于我',
+        link: '/zh-cn/docs/demo1.html',
+      },
+    },
+    users: {
+      title: 'users',
+      desc: <span>some description</span>,
+      list: [
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+      ],
+    },
+  },
+  'en-us': {
+    brand: {
+      brandName: 'Backyard of Charlie Cheng',
+      briefIntroduction: 'AI, Daily Life, myself',
+      buttons: [
+        {
+          text: 'About me',
+          link: '/en-us/docs/demo1.html',
+          type: 'primary',
+        },
+        {
+          text: 'View on Github',
+          link: 'https://github.com/chengtianyue',
+          type: 'normal',
+        },
+      ],
+    },
+    introduction: {
+      title: 'About this website',
+      desc: '',
+      img: '/img/architecture.png',
+    },
+    features: {
+      title: 'Feature List',
+      list: [
+        {
+          img: '/img/feature_transpart.png',
+          title: 'feature1',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_loadbalances.png',
+          title: 'feature2',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_service.png',
+          title: 'feature3',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_hogh.png',
+          title: 'feature4',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_runtime.png',
+          title: 'feature5',
+          content: 'feature description',
+        },
+        {
+          img: '/img/feature_maintenance.png',
+          title: 'feature6',
+          content: 'feature description',
+        }
+      ]
+    },
+    start: {
+      title: 'Quick start',
+      desc: 'some description text',
+      img: '/img/quick_start.png',
+      button: {
+        text: 'READ MORE',
+        link: '/en-us/docs/demo1.html',
+      },
+    },
+    users: {
+      title: 'users',
+      desc: <span>some description</span>,
+      list: [
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+        '/img/users_alibaba.png',
+      ],
+    },
+  },
+};
