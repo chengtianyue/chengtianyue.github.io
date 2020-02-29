13b216f07f29d6a9da091dd1ba1a25c52b820a31
CharlieChengtianyue <tianyue.cheng2002@gmail.com>
2020-03-01 00:54:30 +0800

    community
    deleted ecosystem, added wechat and some urls

--- site_config/community.jsx
+++ site_config/community.jsx
@@ -4,7 +4,7 @@ export default {
   'en-us': {
     barText: 'Community',
     events: {
-      title: 'Events & News',
+      title: 'Latest Blogs',
       list: [
         {
           img: '/img/brhtqqzh.jpeg',
@@ -58,110 +58,58 @@ export default {
       ]
     },
     contacts: {
-      title: 'Talk To Us',
-      desc: 'Feel free to contact us via the following channel.',
+      title: 'Contact Us',
+      desc: 'Feel free to contact me via the following channel.',
       list: [
         {
           img: '/img/mailinglist.png',
           imgHover: '/img/mailinglist_hover.png',
-          title: 'Mailing List',
-          link: ''
+          title: 'Email',
+          link: 'mailto:tianyue.cheng2002@gmail.com'
         },
         {
           img: '/img/alibaba.png',
           imgHover: '/img/alibaba_hover.png',
           title: 'gitter',
-          link: '',
+          link: 'https://gitter.im/charliecheng-ga/community',
         },
         {
-          img: '/img/segmentfault.png',
-          imgHover: '/img/segmentfault_hover.png',
-          title: 'Segment Fault',
-          link: ''
+          img: '/img/wechat.png',
+          imgHover: '/img/wechat_hover.png',
+          title: 'Wechat Public Account',
+          link: '/en-us/docs/wechat_public_qr.html'
         },
         {
           img: '/img/weibo.png',
           imgHover: '/img/weibo_hover.png',
           title: 'weibo',
-          link: '',
+          link: 'https://www.weibo.com/5853994036/profile?rightmod=1&wvr=6&mod=personinfo',
         },
       ],
     },
     contributorGuide: {
       title: 'Contributor Guide',
-      desc: 'some description',
+      desc: 'contact me if anything goes wrong',
       list: [
         {
           img: '/img/mailinglist.png',
-          title: 'Mailing List',
-          content: <span>this is the content</span>,
+          title: 'Email',
+          content: <span>the fastest me to contact me</span>,
         },
         {
           img: '/img/issue.png',
           title: 'Issue',
-          content: <span>this is the content</span>,
+          content: <span>send am issue on Github</span>,
         },
         {
-          img: '/img/documents.png',
-          title: 'Documents',
-          content: <span>this is the content</span>,
+          img: '/img/wechat.png',
+          title: 'Wechat-public-account',
+          content: <span>leave a note at my Wechat-public-account</span>,
         },
         {
           img: '/img/pullrequest.png',
           title: 'Pull Request',
-          content: <span>this is the content</span>,
-        },
-      ],
-    },
-    ecos: {
-      title: 'Eco System',
-      list: [
-        {
-          title: 'Eco1',
-          content: <span>Eco System1</span>,
-          tags: [
-            {
-              text: 'tag1',
-              link: '',
-              bgColor: '#7A63FC',
-            },
-            {
-              text: 'tag2',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-            {
-              text: 'tag3',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-          ],
-        },
-        {
-          title: 'Eco2',
-          content: <span>Eco System2</span>,
-          tags: [
-            {
-              text: 'tag1',
-              link: '',
-              bgColor: '#7A63FC',
-            },
-            {
-              text: 'tag2',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-            {
-              text: 'tag3',
-              link: '/en-us/docs/demo1.html',
-              bgColor: '#00D0D9',
-            },
-            {
-              text: 'tag4',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-          ],
+          content: <span>pull request in github</span>,
         },
       ],
     },
@@ -169,7 +117,7 @@ export default {
   'zh-cn': {
     barText: '社区',
     events: {
-      title: '事件 & 新闻',
+      title: '最新博客',
       list: [
         {
           img: '/img/brhtqqzh.jpeg',
@@ -224,109 +172,57 @@ export default {
     },
     contacts: {
       title: '联系我们',
-      desc: '有问题需要反馈？请通过一下方式联系我们。',
+      desc: '请通过一下方式联系我',
       list: [
         {
           img: '/img/mailinglist.png',
           imgHover: '/img/mailinglist_hover.png',
-          title: '邮件列表',
-          link: ''
+          title: '邮件',
+          link: 'mailto:tianyue.cheng2002@gmail.com'
         },
         {
           img: '/img/alibaba.png',
           imgHover: '/img/alibaba_hover.png',
           title: 'gitter',
-          link: '',
+          link: 'https://gitter.im/charliecheng-ga/community',
         },
         {
-          img: '/img/segmentfault.png',
-          imgHover: '/img/segmentfault_hover.png',
-          title: 'Segment Fault',
-          link: ''
+          img: '/img/wechat.png',
+          imgHover: '/img/wechat_hover.png',
+          title: '微信公众号',
+          link: '/en-us/docs/wechat_public_qr.html'
         },
         {
           img: '/img/weibo.png',
           imgHover: '/img/weibo_hover.png',
           title: '微博',
-          link: '',
+          link: 'https://www.weibo.com/5853994036/profile?rightmod=1&wvr=6&mod=personinfo',
         },
       ],
     },
     contributorGuide: {
-      title: '贡献指南',
-      desc: '一些描述',
+      title: '问题反馈',
+      desc: '可以通过一下渠道向我反映问题',
       list: [
         {
           img: '/img/mailinglist.png',
-          title: '邮件列表',
-          content: <span>这是描述</span>,
+          title: '邮件',
+          content: <span>最快能够联系我的方式</span>,
         },
         {
           img: '/img/issue.png',
-          title: '报告缺陷',
-          content: <span>这是描述</span>,
+          title: '发issue',
+          content: <span>在github上创建issue</span>,
         },
         {
-          img: '/img/documents.png',
-          title: '文档',
-          content: <span>这是描述</span>,
+          img: '/img/wechat.png',
+          title: '微信公众号留言',
+          content: <span>定期会去查看留言</span>,
         },
         {
           img: '/img/pullrequest.png',
           title: 'Pull Request',
-          content: <span>这是描述</span>,
-        },
-      ],
-    },
-    ecos: {
-      title: '生态系统',
-      list: [
-        {
-          title: '生态系统1',
-          content: <span>生态系统1</span>,
-          tags: [
-            {
-              text: '标签1',
-              link: '',
-              bgColor: '#7A63FC',
-            },
-            {
-              text: '标签2',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-            {
-              text: '标签3',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-          ],
-        },
-        {
-          title: '生态系统2',
-          content: <span>生态系统2</span>,
-          tags: [
-            {
-              text: '标签1',
-              link: '',
-              bgColor: '#7A63FC',
-            },
-            {
-              text: '标签2',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-            {
-              text: '标签3',
-              link: '/zh-cn/docs/demo1.html',
-              bgColor: '#00D0D9',
-            },
-            {
-              text: '标签4',
-              link: '',
-              bgColor: '#00D0D9',
-            },
-          ],
+          content: <span>github上发pull request</span>,
         },
       ],
     },
